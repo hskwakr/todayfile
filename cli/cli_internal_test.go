7a464d76bb2c87bced7853bc2c4fc9b9389f187a
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-10-13 00:44:28 +0900

    Create cli_internal_test.go

---
+++ cli/cli_internal_test.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want int
+	}{
+		{
+			name: "case 1: Proper",
+			in:   ApplicationName,
+			want: 0,
+		},
+	}
+
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	app := &CLI{
+		OutStream: outStream,
+		ErrStream: errStream,
+	}
+
+	for _, v := range tests {
+		test := v
+
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			args := strings.Split(test.in, " ")
+			got := app.parse(args)
+			if got != test.want {
+				t.Errorf("ExitStatus: %d, want: %d",
+					got,
+					test.want,
+				)
+			}
+		})
+	}
+}
