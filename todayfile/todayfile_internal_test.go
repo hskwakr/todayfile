73f305dc76fef3d411ee6ea2e6582423769ed060
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-10-14 04:53:41 +0900

    Create todayfile.go and test

---
+++ todayfile/todayfile_internal_test.go
@@ -0,0 +1,38 @@
+package todayfile
+
+import (
+	"testing"
+	"time"
+)
+
+func fakeDate(t time.Time) {
+	now = t
+}
+
+func TestToday(t *testing.T) {
+	tests := []struct {
+		name string
+		in   time.Time
+		want string
+	}{
+		{
+			name: "case 1: Proper",
+			in:   time.Date(1999, time.January, 2, 0, 0, 0, 0, time.UTC),
+			want: "1999-01-02",
+		},
+	}
+
+	for _, v := range tests {
+		test := v
+
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			fakeDate(test.in)
+			got := today()
+			if got != test.want {
+				t.Errorf("got: %v, want: %v", got, test.want)
+			}
+		})
+	}
+}
