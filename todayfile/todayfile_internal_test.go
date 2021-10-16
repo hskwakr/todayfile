6bfcee05b12f9c9bb258a4a19c88b86b5c0d234f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-10-16 18:30:55 +0900

    Update test

--- todayfile/todayfile_internal_test.go
+++ todayfile/todayfile_internal_test.go
@@ -1,6 +1,8 @@
 package todayfile
 
 import (
+	"fmt"
+	"os"
 	"testing"
 	"time"
 )
@@ -27,6 +29,71 @@ func TestDate(t *testing.T) {
 			t.Parallel()
 
 			got := Date(test.in)
+			if got != test.want {
+				t.Errorf("got: %v, want: %v", got, test.want)
+			}
+		})
+	}
+}
+
+func makeFakeFile(name string, msg string) bool {
+	const mode = 0644
+
+	if err := os.WriteFile(name, []byte(msg), mode); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func readFakeFile(name string) string {
+	msg, err := os.ReadFile(name)
+	if err != nil {
+		return fmt.Sprintf("%v", err)
+	}
+
+	return string(msg)
+}
+
+func removeFakeFile(name string) bool {
+	if err := os.Remove(name); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func TestCreate_ShouldNotOverwriteFile(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		{
+			name: "case 1: Proper",
+			in:   "Hello",
+			want: "Hello",
+		},
+	}
+
+	fn := "1992-01-02.txt"
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if success := makeFakeFile(fn, test.in); !success {
+				t.Errorf("failed to creat fake file")
+			}
+
+			if err := Create(fn); err != nil {
+				t.Errorf("%v", err)
+			}
+
+			got := readFakeFile(fn)
+
+			if success := removeFakeFile(fn); !success {
+				t.Errorf("failed to remove fake file")
+			}
+
 			if got != test.want {
 				t.Errorf("got: %v, want: %v", got, test.want)
 			}
